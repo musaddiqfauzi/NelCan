0a10b27b88bfe9442390e2e3cc5c93ce69b06f20
fauzul iman <fauzuliman008@gmail.com>
2019-06-15 01:37:46 +0700

    Update Database v0.2

--- login.php
+++ login.php
@@ -79,15 +79,15 @@
 					
 
 					<!-- Start Sign In Form -->
-					<form action="#" class="fh5co-form animate-box" data-animate-effect="fadeIn">
+					<form action="proseslogin.php" method="post" class="fh5co-form animate-box" data-animate-effect="fadeIn">
 						<h2>Sign In</h2>
 						<div class="form-group">
-							<label for="username" class="sr-only">Username</label>
-							<input type="text" class="form-control" id="username" placeholder="Username" autocomplete="off">
+							<label for="email" class="sr-only">Email</label>
+							<input type="text" class="form-control" id="username" name="email" placeholder="Email" autocomplete="off">
 						</div>
 						<div class="form-group">
 							<label for="password" class="sr-only">Password</label>
-							<input type="password" class="form-control" id="password" placeholder="Password" autocomplete="off">
+							<input type="password" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off">
 						</div>
 						<div class="form-group">
 							<label for="remember"><input type="checkbox" id="remember"> Remember Me</label>
@@ -96,7 +96,7 @@
 							<p>Not registered? <a href="sign-up.html">Sign Up</a> | <a href="forgot.html">Forgot Password?</a></p>
 						</div>
 						<div class="form-group">
-							<input type="submit" value="Sign In" class="btn btn-primary">
+							<input type="submit" value="Sign In" name="login" class="btn btn-primary">
 						</div>
 					</form>
 					<!-- END Sign In Form -->
