7810feb9a055b1a873bd7f3ea0c963e557f84aa3
Musaddiq <musaddiq@mhs.unsyiah.ac.id>
2019-06-16 12:23:41 +0700

    Fix Update v0.07

--- proseslogin.php
+++ proseslogin.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<?php 
+<?php
 session_start();
 require_once("config.php");
 
@@ -13,68 +12,28 @@ if(isset($_POST['login'])){
 
 
     if( mysqli_num_rows($checkuser) == 0) {
-    ?>
+        ?>
         <script language = "JavaScript">
             alert("email tidak terdaftar..");
             document.location='login.php';
-            </script>
+        </script>
         <?php
     } else {
         if($password != $hasil['password']) {
-        ?>
-        <script language = "JavaScript">
-            alert("email atau Password Anda Salah..");
-            document.location='login.php';
+            ?>
+            <script language = "JavaScript">
+                alert("email atau Password Anda Salah..");
+                document.location='login.php';
             </script>
-        <?php
+            <?php
         } else {
             $_SESSION['email'] = $hasil['email'];
             $_SESSION['password']= $hasil['password'];
-                header('location:index.html');
+            header('location: Laman-tabel-ikan.html');
         }
     }
 } else {
     die("Akses dilarang...");
 }
 
-=======
-<?php 
-session_start();
-require_once("config.php");
-
-if(isset($_POST['login'])){
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $sql      = "SELECT * FROM daftar_pengguna WHERE email = '$email'";
-    $checkuser  = mysqli_query($db, $sql);
-    $hasil    = mysqli_fetch_array($checkuser);
-
-
-    if( mysqli_num_rows($checkuser) == 0) {
-    ?>
-        <script language = "JavaScript">
-            alert("email tidak terdaftar..");
-            document.location='login.php';
-            </script>
-        <?php
-    } else {
-        if($password != $hasil['password']) {
-        ?>
-        <script language = "JavaScript">
-            alert("email atau Password Anda Salah..");
-            document.location='login.php';
-            </script>
-        <?php
-        } else {
-            $_SESSION['email'] = $hasil['email'];
-            $_SESSION['password']= $hasil['password'];
-                header('location: Laman-tabel-ikan.html');
-        }
-    }
-} else {
-    die("Akses dilarang...");
-}
-
->>>>>>> Musaddiq
 ?>
